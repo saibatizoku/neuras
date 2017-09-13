002701eaf884cce17b43075e099684d085dd8c95
Joaquin Rosales <globojorro@gmail.com>
2017-09-13 12:55:18 -0500

    convenience re-export of 'zmq'

--- src/lib.rs
+++ src/lib.rs
@@ -3,11 +3,9 @@
 #[macro_use]
 extern crate error_chain;
 extern crate url;
-extern crate zmq;
+pub extern crate zmq;
 
 /// Error handling.
 pub mod errors;
 /// Usueful utilities to deal with ZMQ.
 pub mod utils;
-
-pub use zmq::{Context, CurveKeyPair, Message, Socket};
