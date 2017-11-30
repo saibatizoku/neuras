8468997d080dc9c8ac5d80ded9020fe707ae0cf1
Joaquin Rosales <globojorro@gmail.com>
2017-11-29 21:29:47 -0600

    pub extern crate for consumers to use

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,10 @@
 #[macro_use]
 extern crate error_chain;
 pub extern crate url;
+pub extern crate tokio_core;
+pub extern crate futures;
 pub extern crate zmq;
+pub extern crate zmq_tokio;
 
 /// Error handling.
 pub mod errors;
