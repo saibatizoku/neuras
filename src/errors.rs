86f3c55cf0b3c3ef90491d85a80a4b0934f16142
Joaquin Rosales <globojorro@gmail.com>
2017-12-13 00:02:19 -0600

    docs for main errors module

--- src/errors.rs
+++ src/errors.rs
@@ -1,3 +1,11 @@
+//! One-point stop for error-handling.
+//!
+//! Error handling is done using `error-chain` to link module-level
+//! errors. If you are only using specific modules from this crate,
+//! it might be better to use module-level errors.
+//!
+//! Check the documentation of the module that you are using to learn
+//! more about the errors it handles.
 use std::io;
 
 use zmq;
