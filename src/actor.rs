fe755d49c26c3774374568bac60b9736c7e9c3b6
Joaquin Rosales <globojorro@gmail.com>
2018-01-06 14:17:07 -0600

    tweaking of src/actor.rs

--- src/actor.rs
+++ src/actor.rs
@@ -23,6 +23,9 @@ pub mod errors {
     use socket;
     error_chain! {
         errors {
+            AddressNotSpecified {
+                description("the actorling needs to have an addess to work")
+            }
             NotStarted {
                 description("actorling is not started")
             }
@@ -40,39 +43,38 @@ pub mod errors {
     }
 }
 
-use std::collections::HashMap;
 use std::thread;
 use std::time::Duration;
 
 use futures::{Future, Sink, Stream};
-use futures::future::{loop_fn, ok, FutureResult, Loop};
 use tokio_core::reactor::{Core, Handle};
 use tokio_signal;
 use uuid::{Uuid, NAMESPACE_DNS};
 use zmq;
 
-use socket::{socket_new_pair, Socket};
+use socket::Socket;
 
 use self::errors::*;
 
+#[allow(dead_code)]
+/// A base type for actor-like entities
 pub struct Actorling {
     address: String,
     context: zmq::Context,
-    connections: HashMap<String, String>,
     pipe: Socket,
     uuid: Uuid,
 }
 
 impl Actorling {
+    /// Create a new `Actorling` instance with the address that it will be known for within
+    /// the network.
     pub fn new(addr: &str) -> Result<Self> {
-        let address = addr.to_string();
         let context = zmq::Context::new();
-        let pipe = Socket::new(zmq::PAIR)?;
+        let pipe = Socket::new(zmq::PAIR).unwrap();
         let uuid = Uuid::new_v5(&NAMESPACE_DNS, "actorling");
-        let connections = HashMap::new();
+        let _ = pipe.bind(&uuid_pipe_address(&uuid))?;
         let actorling = Actorling {
-            address,
-            connections,
+            address: addr.to_string(),
             context,
             pipe,
             uuid,
@@ -80,39 +82,51 @@ impl Actorling {
         Ok(actorling)
     }
 
+    /// Create a new `Actorling` instance that shares network context with the creator.
+    /// Useful for creating actors in the same process (possibly/commonly in child threads),
+    /// that can talk to the creator actor (usually running on the main thread, but could be
+    /// run from a child thread as well).
+    pub fn new_sibling(addr: &str, context: zmq::Context) -> Result<Self> {
+        let address = addr.to_string();
+        let pipe = Socket::new(zmq::PAIR).unwrap();
+        let uuid = Uuid::new_v5(&NAMESPACE_DNS, "actorling");
+        let _ = pipe.bind(&uuid_pipe_address(&uuid))?;
+        let actorling = Actorling {
+            address,
+            context,
+            pipe,
+            uuid,
+        };
+        Ok(actorling)
+    }
+}
+
+impl Actorling {
+    /// Returns a `String` with the address for the Actorling.
     pub fn address(&self) -> String {
         self.address.clone()
     }
 
+    /// Returns the actorling's network context.
+    /// Useful for spawning sockets, and for creating sibling actors (see
+    /// `Actorling::new_sibling`).
     pub fn context(&self) -> zmq::Context {
         self.context.clone()
     }
 
+    /// Returns a `String` the IPC address for this `Actorling`.
     pub fn pipe_address(&self) -> String {
-        format!("inproc://{}", self.uuid.simple().to_string())
+        uuid_pipe_address(&self.uuid)
     }
 
-    fn recv_msg(&self) -> FutureResult<String, Error> {
-        ok("fake msg".to_string())
-    }
-
-    fn setup_thread(&self) -> Result<thread::Builder> {
-        let builder = thread::Builder::new();
-        Ok(builder)
-    }
-
+    /// Function for spawing child-threads, returning the `thread::JoinHandle`.
     pub fn run_thread<F, T>(&self, name: &str, callback: F) -> Result<thread::JoinHandle<T>>
     where
         F: FnOnce() -> T,
         F: Send + 'static,
         T: Send + 'static,
     {
-        let thred = self.setup_thread()?;
-        let handler = thred
-            .name(name.to_string())
-            .spawn(callback)
-            .chain_err(|| "could not spawn actorling thread");
-        handler
+        run_thread(name, callback)
     }
 
     pub fn run(&self) -> Result<()> {
@@ -185,10 +199,12 @@ impl Actorling {
         Ok(())
     }
 
-    pub fn start(&self) -> Result<String> {
+    /// Start the current actorling instance.
+    pub fn start(&self) -> Result<()> {
         unimplemented!();
     }
 
+    /// Stops the current actorling instance.
     pub fn stop(&self) -> Result<()> {
         unimplemented!();
     }
@@ -199,7 +215,25 @@ impl Actorling {
     }
 }
 
-fn start_pipe_server(
+/// Function for spawing child-threads, returning the `thread::JoinHandle`.
+pub fn run_thread<F, T>(name: &str, callback: F) -> Result<thread::JoinHandle<T>>
+where
+    F: FnOnce() -> T,
+    F: Send + 'static,
+    T: Send + 'static,
+{
+    let handler = thread::Builder::new()
+        .name(name.to_string())
+        .spawn(callback)
+        .chain_err(|| "could not spawn actorling thread");
+    handler
+}
+
+fn uuid_pipe_address(uuid: &Uuid) -> String {
+    format!("inproc://{}", uuid.simple().to_string())
+}
+
+fn _start_pipe_server(
     pipe: Socket,
     handle: &Handle,
 ) -> Box<Future<Item = (), Error = ::std::io::Error> + ::std::marker::Send + 'static> {
@@ -209,7 +243,7 @@ fn start_pipe_server(
         rx.take(1)
             .fold(tx, |tx, req| {
                 println!("got REQ {:?}", req);
-                tx.send(req[0].clone())
+                tx.send(req)
             })
             .map(|_| {}),
     )
@@ -228,7 +262,7 @@ mod tests {
     #[test]
     fn actorlings_return_pipe_address_on_start() {
         let acty = Actorling::new("inproc://my_actorling").unwrap();
-        let pipe_addr = acty.start().unwrap();
+        let pipe_addr = acty.pipe_address();
         let ep = format!("inproc://{}", acty.uuid.simple().to_string());
         assert_eq!(pipe_addr, ep);
     }
@@ -236,7 +270,7 @@ mod tests {
     #[test]
     fn actorlings_return_ok_on_stop() {
         let acty = Actorling::new("inproc://my_actorling").unwrap();
-        let ep = acty.start().unwrap();
+        let _ = acty.start().unwrap();
         let stop = acty.stop();
         assert!(stop.is_ok());
     }
