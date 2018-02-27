9412bd7aba731c1f7ff2760c21a6259cd34f1bc6
Joaquin Rosales <globojorro@gmail.com>
2018-02-27 12:29:53 -0600

    change PollableSocket to take ownership of low-level socket

--- src/socket_mio.rs
+++ src/socket_mio.rs
@@ -1,21 +1,33 @@
-//! `mio`-compatibility for sockets.
+//! Non-blocking sockets for polling.
+//!
+//! `PollableSocket` implements `SocketSend`, and `SocketRecv` by sending
+//! the `zmq::DONTWAIT` flag, and returning `std::io::Error`.
+//!
+//! Users of `PollableSocket` must handle the case when the result is an error
+//! with `std::io::ErrorKind::WouldBlock`, as per-usual when dealing with
+//! non-blocking (asynchronous) code.
+//!
+//! This module also adds `mio`-compatibility for sockets, by implementing
+//! the `mio::Evented` trait, which is used for registering the
+//! socket with a `mio::Poll` instance.
 use super::{SocketRecv, SocketSend, SocketWrapper};
 
 use std::io;
 use std::os::unix::io::RawFd;
 
+use mio_lib::Evented;
+use mio_lib::{Events, Poll, PollOpt, Ready, Token};
 use mio_lib::unix::EventedFd;
-use mio_lib::{Evented, Poll, PollOpt, Ready, Token};
 use zmq::{Message, Sendable, Socket, DONTWAIT};
 
 /// Pollable wrapper for sockets.
-pub struct PollableSocket<'a> {
-    inner: &'a Socket,
+pub struct PollableSocket {
+    inner: Socket,
 }
 
-impl<'a> PollableSocket<'a> {
+impl PollableSocket {
     /// Create a new `PollableSocket` instance.
-    pub fn new(inner: &'a Socket) -> PollableSocket {
+    pub fn new(inner: Socket) -> PollableSocket {
         PollableSocket { inner }
     }
 
@@ -27,9 +39,9 @@ impl<'a> PollableSocket<'a> {
 }
 
 /// Implementation of the `SocketWrapper` API for pollable sockets.
-impl<'a> SocketWrapper for PollableSocket<'a> {
+impl SocketWrapper for PollableSocket {
     fn get_socket_ref(&self) -> &Socket {
-        self.inner
+        &self.inner
     }
     fn get_rcvmore(&self) -> io::Result<bool> {
         self.get_socket_ref().get_rcvmore().map_err(|e| e.into())
@@ -37,7 +49,7 @@ impl<'a> SocketWrapper for PollableSocket<'a> {
 }
 
 /// Implementation of the `SocketSend` API for pollable sockets.
-impl<'a> SocketSend for PollableSocket<'a> {
+impl SocketSend for PollableSocket {
     fn send<M>(&self, msg: M, flags: i32) -> io::Result<()>
     where
         M: Sendable,
@@ -59,7 +71,7 @@ impl<'a> SocketSend for PollableSocket<'a> {
 }
 
 /// Implementation of the `SocketRecv` API for pollable sockets.
-impl<'a> SocketRecv for PollableSocket<'a> {
+impl SocketRecv for PollableSocket {
     fn recv(&self, buf: &mut Message, flags: i32) -> io::Result<()> {
         self.get_socket_ref()
             .recv(buf, DONTWAIT | flags)
@@ -97,70 +109,47 @@ impl<'a> SocketRecv for PollableSocket<'a> {
     }
 }
 
-/// Implementation of the external `mio::Evented` API for pollable sockets.
-impl<'a> Evented for PollableSocket<'a> {
-    fn register(
-        &self,
-        poll: &Poll,
-        token: Token,
-        interest: Ready,
-        opts: PollOpt,
-    ) -> io::Result<()> {
-        let fd = try!(self.as_fd());
-        EventedFd(&fd).register(poll, token, interest, opts)
-    }
-
-    fn reregister(
-        &self,
-        poll: &Poll,
-        token: Token,
-        interest: Ready,
-        opts: PollOpt,
-    ) -> io::Result<()> {
-        let fd = try!(self.as_fd());
-        EventedFd(&fd).reregister(poll, token, interest, opts)
-    }
-
-    fn deregister(&self, poll: &Poll) -> io::Result<()> {
-        let fd = try!(self.as_fd());
-        EventedFd(&fd).deregister(poll)
-    }
-}
-
-/// Implementation of the external `mio::Evented` API for pollable socket references.
-impl<'a, 'b> Evented for &'b PollableSocket<'a> {
-    fn register(
-        &self,
-        poll: &Poll,
-        token: Token,
-        interest: Ready,
-        opts: PollOpt,
-    ) -> io::Result<()> {
-        let fd = (*self).as_fd()?;
-        EventedFd(&fd).register(poll, token, interest, opts)
-    }
-
-    fn reregister(
-        &self,
-        poll: &Poll,
-        token: Token,
-        interest: Ready,
-        opts: PollOpt,
-    ) -> io::Result<()> {
-        let fd = try!((*self).as_fd());
-        EventedFd(&fd).reregister(poll, token, interest, opts)
-    }
-
-    fn deregister(&self, poll: &Poll) -> io::Result<()> {
-        let fd = try!((*self).as_fd());
-        EventedFd(&fd).deregister(poll)
-    }
-}
-
-/// Converts from a socket reference into a pollable socket.
-impl<'a> From<&'a Socket> for PollableSocket<'a> {
-    fn from(socket: &'a Socket) -> Self {
+/// Converts from a regular socket into a pollable socket.
+impl From<Socket> for PollableSocket {
+    fn from(socket: Socket) -> Self {
         PollableSocket::new(socket)
+    }
+}
+
+/// Converts from a pollable socket into a regular socket.
+impl Into<Socket> for PollableSocket {
+    fn into(self) -> Socket {
+        self.inner
+    }
+}
+
+/// Implementation of the external `mio::Evented` API for pollable sockets.
+impl Evented for PollableSocket {
+    fn register(
+        &self,
+        poll: &Poll,
+        token: Token,
+        interest: Ready,
+        opts: PollOpt,
+    ) -> io::Result<()> {
+        let fd = try!(self.as_fd());
+        EventedFd(&fd).register(poll, token, interest, opts)
+    }
+
+    fn reregister(
+        &self,
+        poll: &Poll,
+        token: Token,
+        interest: Ready,
+        opts: PollOpt,
+    ) -> io::Result<()> {
+        let fd = try!(self.as_fd());
+        EventedFd(&fd).reregister(poll, token, interest, opts)
+    }
+
+    fn deregister(&self, poll: &Poll) -> io::Result<()> {
+        let fd = try!(self.as_fd());
+        EventedFd(&fd).deregister(poll)
     }
 }
 
@@ -177,16 +166,16 @@ mod tests {
     }
 
     #[test]
-    fn new_mio_socket_wraps_reference_to_zmq_socket() {
+    fn new_pollable_socket_wraps_reference_to_zmq_socket() {
         let socket = setup_socket();
-        let evented = PollableSocket::new(&socket);
-        assert_eq!(evented.inner.get_identity(), socket.get_identity());
+        let pollable = PollableSocket::new(socket);
+        assert_eq!(pollable.inner.get_identity(), Ok(b"my_identity".to_vec()));
     }
 
     #[test]
-    fn convert_from_zmq_socket_reference_to_mio_socket() {
+    fn convert_from_zmq_socket_reference_to_pollable_socket() {
         let socket = setup_socket();
-        let evented: PollableSocket = (&socket).into();
-        assert_eq!(evented.inner.get_identity(), socket.get_identity());
+        let pollable: PollableSocket = socket.into();
+        assert_eq!(pollable.inner.get_identity(), Ok(b"my_identity".to_vec()));
     }
 }
