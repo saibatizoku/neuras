6424c33992da74848c0b7555aa3df13b7b0c8b35
Joaquin Rosales <globojorro@gmail.com>
2018-02-27 22:47:29 -0600

    add basic tests for Poller

--- src/poller.rs
+++ src/poller.rs
@@ -1,1 +1,27 @@
-//! [WIP] Polling for sockets.
+//! Polling for evented actor types.
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use zmq;
+
+    #[test]
+    fn default_poller_has_no_pre_allocation() {
+        let poller: Poller = Default::default();
+        assert_eq!(poller.actors.capacity(), 0);
+    }
+
+    #[test]
+    fn new_poller_can_be_created_with_capacity() {
+        let poller: Poller = Poller::with_capacity(20);
+        assert_eq!(poller.actors.capacity(), 20);
+    }
+
+    #[test]
+    fn new_poller_can_be_created_with_existing_context_and_capacity() {
+        let context = zmq::Context::new();
+        let ctx = context.clone();
+        let poller: Poller = Poller::with_context_and_capacity(ctx, 30);
+        assert_eq!(poller.actors.capacity(), 30);
+    }
+}
